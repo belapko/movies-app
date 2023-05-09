8bee306466d04928f5986b09d1c250c0ec7d18ba
belapko <belapko23@gmail.com>
2023-05-09 17:04:58 +0300

    created movies list display

--- movies/pages/index.tsx
+++ movies/pages/index.tsx
@@ -1,6 +1,9 @@
 import {getSession} from "next-auth/react";
 import {NextPageContext} from "next";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -19,9 +22,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+    const {data: movies = []} = useMovieList();
+
     return (
         <>
             <Navbar/>
+            <Billboard/>
+            <div className='pb-40'>
+                <MovieList title='Trending now' data={movies}/>
+            </div>
         </>
     )
 }
