55ba00192174afaad5c29ba2270a9ba719ed0f49
belapko <belapko23@gmail.com>
2023-05-13 02:12:03 +0300

    fixed double header in auth component

--- movies/pages/_app.tsx
+++ movies/pages/_app.tsx
@@ -6,10 +6,12 @@ import Navbar from "@/components/Navbar";
 import InfoModal from "@/components/infoModal";
 import useInfoModal from "@/hooks/useInfoModal";
 import Head from "next/head";
+import {useRouter} from "next/router";
 
 export default function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
     const {isOpen, closeModal} = useInfoModal();
-    const locationPathname = location.pathname
+    const router = useRouter();
+    const locationPathname = router.pathname;
 
     return (
         <SessionProvider session={session}>
@@ -18,8 +20,12 @@ export default function App({Component, pageProps: {session, ...pageProps}}: App
                     {locationPathname === '/' ? 'Movies' : locationPathname.charAt(1).toUpperCase() + locationPathname.slice(2)}
                 </title>
             </Head>
+            {locationPathname === '/auth' ? null :
+                <>
             <InfoModal visible={isOpen} onClose={closeModal}/>
             <Navbar/>
+                </>
+            }
             <Component {...pageProps}/>
         </SessionProvider>
     )
